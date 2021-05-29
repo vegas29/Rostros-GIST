3d7b4452fa497663a5f7fd836a7495fce66cc5c4
Alejo29 <diegoleandro-29@hotmail.com>
2021-05-29 12:25:46 -0500

    Crear-Desaparecido Commit
    Crear-Desaparecido Ready

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+"use strict";
+
+//Accordion
+
 const acc = document.getElementsByClassName("accordion");
 let i;
 
@@ -12,3 +16,27 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+//Tabs
+
+function openCity(evt, cityName) {
+
+  let i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("actives", "");
+  }
+
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += "actives tablinks";
+}
+
+document.getElementById("defaultOpen").click();
